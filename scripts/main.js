76a875c66b8b2bbacc0347ac665c86394a9ff1bc
Leo Chung <leochungus@berkeley.edu>
2021-07-26 21:39:54 -0700

    added svg + added random img on load at title

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+var images = ['images/shops.svg', 'images/calm.svg'];
+
+/* Picks a random image and displays it in the randomImage <img> tag. */
+function displayRandomImage() {
+    document.getElementById("random-display").src = images[Math.floor(Math.random() * images.length)];
+}
+displayRandomImage();
